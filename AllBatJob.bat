b7473f7be41303f99404badc1caa5b14c801b701
shivasundar07 <shivasundar.dk@excelindia.com>
2021-09-23 15:20:19 +0530

    AllJob

---
+++ AllBatJob.bat
@@ -0,0 +1,9 @@
+@echo off
+md D:\Jenkins_Practice\Directory_Creation\File111
+md D:\Jenkins_Practice\Directory_Creation\File222
+
+@echo
+Copy D:\Jenkins_Practice\Copy\1 D:\Jenkins_Practice\Copy\2
+
+@echo
+move D:\Jenkins_Practice\Move\Newfolder1 D:\Jenkins_Practice\Move\Newfolder2
