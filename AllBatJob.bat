eb2e25f95aabbf6f1c2901d4ca1403ba79131f8a
shivasundar07 <79079829+shivasundar07@users.noreply.github.com>
2021-09-23 15:27:59 +0530

    Update AllBatJob.bat

--- AllBatJob.bat
+++ AllBatJob.bat
@@ -2,8 +2,8 @@
 md D:\Jenkins_Practice\Directory_Creation\File111
 md D:\Jenkins_Practice\Directory_Creation\File222
 
-@echo
+@echo off
 Copy D:\Jenkins_Practice\Copy\1 D:\Jenkins_Practice\Copy\2
 
-@echo
+@echo off
 move D:\Jenkins_Practice\Move\Newfolder1 D:\Jenkins_Practice\Move\Newfolder2
